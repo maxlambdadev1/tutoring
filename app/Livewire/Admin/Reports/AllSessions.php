4d4a922d187ffae1e9594e184cd1743effae1225
maxlambdadev1 <berishvilitemoraz01@gmail.com>
2024-06-26 10:13:26 -0400

    add all-sessions component

---
+++ app/Livewire/Admin/Reports/AllSessions.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Admin\Reports;
+
+use Livewire\Component;
+
+class AllSessions extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.reports.all-sessions');
+    }
+}
