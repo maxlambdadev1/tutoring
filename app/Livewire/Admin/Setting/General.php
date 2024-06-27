2b346b067c019601d8f24103b76f260620e474d4
maxlambdadev1 <berishvilitemoraz01@gmail.com>
2024-06-27 15:31:53 -0400

    general settings page

--- app/Livewire/Admin/Setting/General.php
+++ app/Livewire/Admin/Setting/General.php
@@ -5,7 +5,9 @@ namespace App\Livewire\Admin\Setting;
 use App\Models\Announcement;
 use App\Models\State;
 use App\Models\Tutor;
+use App\Models\Option;
 use App\Models\AlchemyParent;
+use App\Trait\Functions;
 use App\Trait\Mailable;
 use Illuminate\Support\Facades\DB;
 use Livewire\Attributes\Layout;
@@ -14,7 +16,8 @@ use Livewire\Component;
 #[Layout('admin.layouts.app')]
 class General extends Component
 {
-    use Mailable;
+    use Mailable, Functions;
+
     public $announcements;
     public $states;
 
@@ -28,7 +31,15 @@ class General extends Component
     public $parent_sms_who = 1;
     public $parent_sms_state; //state string
 
-    public function mount() {
+    public $job_switch = false;
+    public $online_limit;
+    public $experience_limit;
+    public $daily_target;
+    public $cron_time;
+
+
+    public function mount()
+    {
         $this->announcements = Announcement::get();
         $this->states = State::get();
         $this->announcement_text = $this->announcements[0]->an_text;
@@ -36,16 +47,22 @@ class General extends Component
         $this->tutor_sms_announcement_text = $this->announcements[1]->an_text;
         $this->tutor_sms_flag = $this->announcements[1]->flag ? true : false;
         $this->tutor_sms_who = $this->announcements[1]->who;
-        $this->tutor_sms_state = $this->announcements[1]->state; 
+        $this->tutor_sms_state = $this->announcements[1]->state;
 
         $this->parent_sms_announcement_text = $this->announcements[2]->an_text;
         $this->parent_sms_flag = $this->announcements[2]->flag ? true : false;
         $this->parent_sms_who = $this->announcements[2]->who;
-        $this->parent_sms_state = $this->announcements[2]->state; 
+        $this->parent_sms_state = $this->announcements[2]->state;
+
+        $this->job_switch = $this->getOption('job-status') ? true : false;
+        $this->online_limit = $this->getOption('online-limit') ?? 0;
+        $this->experience_limit = $this->getOption('experience-limit') ?? 0;
+        $this->daily_target = unserialize($this->getOption('daily-target')) ?? [];
+        $this->cron_time = unserialize($this->getOption('cron-time')) ?? [];
     }
 
     public function saveAnnouncement()
-    { 
+    {
         try {
             if (!empty($this->announcement_text)) {
                 $announcement = Announcement::find(1);
@@ -61,7 +78,6 @@ class General extends Component
                     'message' => 'Settings saved!'
                 ]);
             } else throw new \Exception("Please input the announcement.");
-
         } catch (\Exception $e) {
             $this->dispatch('showToastrMessage', [
                 'status' => 'error',
@@ -69,9 +85,9 @@ class General extends Component
             ]);
         }
     }
-    
+
     public function saveTutorAnnouncementSms()
-    { 
+    {
         try {
             if (!empty($this->tutor_sms_announcement_text)) {
                 $this->tutor_sms_flag = true;
@@ -108,7 +124,6 @@ class General extends Component
                     'message' => 'Sent SMS to tutors'
                 ]);
             } else throw new \Exception("Please input the announcement.");
-
         } catch (\Exception $e) {
             $this->tutor_sms_flag = false;
             DB::rollBack();
@@ -140,16 +155,16 @@ class General extends Component
 
                 if ($this->parent_sms_who == 2) $query = $query->whereExists(function ($query) {
                     $query->select(DB::raw(1))
-                          ->from('alchemy_children as c')
-                          ->whereRaw('c.parent_id = alchemy_parent.id')
-                          ->where('c.child_status', '>', 0);
+                        ->from('alchemy_children as c')
+                        ->whereRaw('c.parent_id = alchemy_parent.id')
+                        ->where('c.child_status', '>', 0);
                 }); //active parent
                 else if ($this->parent_sms_who == 3) $query = $query->whereNotExists(function ($query) {
-                        $query->select(DB::raw(1))
-                            ->from('alchemy_children as c')
-                            ->whereRaw('c.parent_id = alchemy_parent.id')
-                            ->where('c.child_status', '>', 0);
-                    }); //inactive parent
+                    $query->select(DB::raw(1))
+                        ->from('alchemy_children as c')
+                        ->whereRaw('c.parent_id = alchemy_parent.id')
+                        ->where('c.child_status', '>', 0);
+                }); //inactive parent
                 else if ($this->parent_sms_who == 4) $query = $query->where('stripe_customer_id', 'like', '%cus_%')
                     ->where('created_at', '>=', date('Y') . '-01-01')
                     ->whereNotExists(function ($query) {
@@ -176,10 +191,109 @@ class General extends Component
                     'message' => 'Sent SMS to parents'
                 ]);
             } else throw new \Exception("Please input the announcement.");
-
         } catch (\Exception $e) {
             $this->parent_sms_flag = 0;
             DB::rollBack();
+            $this->dispatch('showToastrMessage', [
+                'status' => 'error',
+                'message' => $e->getMessage()
+            ]);
+        }
+    }
+
+    public function changeJobSwitch()
+    {
+        try {
+            $option = Option::where('option_name', 'job-status')->first();
+            $option->update([
+                'option_value' => $this->job_switch,
+            ]);
+
+            $this->dispatch('showToastrMessage', [
+                'status' => 'success',
+                'message' => 'Settings saved!'
+            ]);
+        } catch (\Exception $e) {
+            $this->dispatch('showToastrMessage', [
+                'status' => 'error',
+                'message' => $e->getMessage()
+            ]);
+        }
+    }
+    
+    public function saveOnlineLessonLimit()
+    {
+        try {
+            $option = Option::where('option_name', 'online-limit')->first();
+            $option->update([
+                'option_value' => $this->online_limit,
+            ]);
+
+            $this->dispatch('showToastrMessage', [
+                'status' => 'success',
+                'message' => 'Settings saved!'
+            ]);
+        } catch (\Exception $e) {
+            $this->dispatch('showToastrMessage', [
+                'status' => 'error',
+                'message' => $e->getMessage()
+            ]);
+        }
+    }
+    
+    public function saveExperiencedTutorLimit()
+    {
+        try {
+            $option = Option::where('option_name', 'experience-limit')->first();
+            $option->update([
+                'option_value' => $this->experience_limit,
+            ]);
+
+            $this->dispatch('showToastrMessage', [
+                'status' => 'success',
+                'message' => 'Settings saved!'
+            ]);
+        } catch (\Exception $e) {
+            $this->dispatch('showToastrMessage', [
+                'status' => 'error',
+                'message' => $e->getMessage()
+            ]);
+        }
+    }
+    
+    public function saveDailyTarget()
+    {
+        try {
+            $option = Option::where('option_name', 'daily-target')->first();
+            $option->update([
+                'option_value' => serialize($this->daily_target),
+            ]);
+
+            $this->dispatch('showToastrMessage', [
+                'status' => 'success',
+                'message' => 'Settings saved!'
+            ]);
+        } catch (\Exception $e) {
+            $this->dispatch('showToastrMessage', [
+                'status' => 'error',
+                'message' => $e->getMessage()
+            ]);
+        }
+    }
+        
+    public function saveCronTime()
+    {
+        try {
+            $option = Option::where('option_name', 'cron-time')->first();
+            $option->update([
+                'option_value' => serialize($this->cron_time),
+            ]);
+
+            $this->dispatch('showToastrMessage', [
+                'status' => 'success',
+                'message' => 'Settings saved!'
+            ]);
+        } catch (\Exception $e) {
             $this->dispatch('showToastrMessage', [
                 'status' => 'error',
                 'message' => $e->getMessage()
