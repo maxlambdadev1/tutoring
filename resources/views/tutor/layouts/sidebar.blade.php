c6eec6874427924dd8f2d6662dcd6578bd80750d
maxlambdadev1 <berishvilitemoraz01@gmail.com>
2024-07-05 15:45:31 -0400

    all-jobs page

--- resources/views/tutor/layouts/sidebar.blade.php
+++ resources/views/tutor/layouts/sidebar.blade.php
@@ -8,6 +8,21 @@
     </li>
     
     <li class="side-nav-item">
+        <a data-bs-toggle="collapse" href="#sidebarJobs" aria-expanded="false" aria-controls="sidebarJobs" class="side-nav-link">
+            <i class="uil  uil-map-pin-alt"></i>
+            <span> Jobs </span>
+            <span class="menu-arrow"></span>
+        </a>
+        <div class="collapse" id="sidebarJobs">
+            <ul class="side-nav-second-level">
+                <li>
+                    <a href="{{ route('tutor.jobs.all-jobs') }}" wire:navigate>All jobs</a>
+                </li>
+            </ul>
+        </div>
+    </li>
+
+    <li class="side-nav-item">
         <a data-bs-toggle="collapse" href="#sessions" aria-expanded="false" aria-controls="sidebarThirdparty" class="side-nav-link">
             <i class="uil  uil-calendar-alt"></i>
             <span> Sessions </span>
@@ -24,4 +39,5 @@
             </ul>
         </div>
     </li>
+    
 </ul>
