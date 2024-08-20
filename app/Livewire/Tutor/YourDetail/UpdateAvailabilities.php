7b1191341634d694b1ee89a9608cf36107808abf
maxlambdadev1 <berishvilitemoraz01@gmail.com>
2024-08-19 20:21:35 -0400

    update availabilities/subjects

--- app/Livewire/Tutor/YourDetail/UpdateAvailabilities.php
+++ app/Livewire/Tutor/YourDetail/UpdateAvailabilities.php
@@ -4,55 +4,29 @@ namespace App\Livewire\Tutor\YourDetail;
 
 use App\Models\Availability;
 use App\Trait\Functions;
+use App\Trait\WithLeads;
 use Livewire\Attributes\Layout;
 use Livewire\Component;
 
 #[Layout('tutor.layouts.app')]
 class UpdateAvailabilities extends Component
 {
-    use Functions;
+    use Functions, WithLeads;
 
     public $tutor;
     public $availabilities = [];
     public $total_availabilities = [];
-    public $is_selected_all = true;
 
     public function mount() {
         $tutor = auth()->user()->tutor;
         $this->tutor = $tutor;
         $this->availabilities = $this->getAvailabilitiesFromString($tutor->availabilities) ?? [];
-        if (empty($this->availabilities)) $this->is_selected_all = false;
         $this->total_availabilities = Availability::get();
     }
 
-    public function toggleAvailItemStatus($avail_hour) {
-        if (in_array($avail_hour, $this->availabilities)) {
-            $this->availabilities = array_filter($this->availabilities, function ($value) use ($avail_hour) {
-                return $value != $avail_hour; 
-            });
-        } else {
-            $this->availabilities[] = $avail_hour;
-        }
-    }
-
-    public function selectAllAvailabilities($flag) {
-        if ($flag) {
-            $this->availabilities = [];
-            foreach ($this->total_availabilities as $item) {
-                foreach ($item->getAvailabilitiesName() as $ele) {
-                    $avail_hour = $item->short_name . '-' . $ele; //mon-7:00 AM
-                    $this->availabilities[] = $avail_hour;
-                }
-            }
-            $this->is_selected_all = true;
-        } else {
-            $this->availabilities = [];
-            $this->is_selected_all = false;
-        }
-    }
-
-    public function updateAvailabilities() {
+    public function updateAvailabilities($availabilities) {
         try {
+            $this->availabilities = $this->orderAvailabilitiesAccordingToDay($availabilities);
             $av_str = $this->generateBookingAvailability($this->availabilities);
             $this->tutor->update([
                 'availabilities' => $av_str
--- resources/views/livewire/tutor/your-detail/update-availabilities.blade.php
+++ resources/views/livewire/tutor/your-detail/update-availabilities.blade.php
@@ -5,37 +5,45 @@
     @endphp
     <x-custom-header :title="$title" :breadcrumbs="$breadcrumbs" />
 
-    <div class="row">
+    <div class="row" x-data="update_availabilities_init">
         <div class="col-12">
             <div class="card mb-3">
                 <div class="card-body">
                     <div class="row mb-3">
                         <div class="col-md-2"></div>
                         <div class="col-md-4">
-                            <button type="button" class="btn btn{{ $is_selected_all ? '' : '-outline'}}-warning waves-effect waves-light w-100" wire:click="selectAllAvailabilities(true)">I am completely available</button>
+                            <button type="button" class="btn btn-warning waves-effect waves-light w-100" x-on:click="selectAllAvailabilities(true)">I am completely available</button>
                         </div>
                         <div class="col-md-4">
-                            <button type="button" class="btn btn{{ $is_selected_all ? '-outline' : ''}}-secondary waves-effect waves-light w-100"wire:click="selectAllAvailabilities(false)">I am completely unavailable</button>
+                            <button type="button" class="btn btn-secondary waves-effect waves-light w-100" x-on:click="selectAllAvailabilities(false)">I am completely unavailable</button>
                         </div>
                     </div>
-                    <div class="availabilities_wrapper">
-                        <div class="row mb-3">
+                    <div class="availabilities_wrapper mb-4 px-3">
+                        <div class="row">
+                            @php $i = 0; $total_av = []; @endphp
                             @foreach ($total_availabilities as $item)
                             <div class="col-md text-center" data-day="{{$item->name}}">
                                 <h5>{{$item->name}}</h5>
                                 @forelse ($item->getAvailabilitiesName() as $ele)
-                                @php $avail_hour = $item->short_name . '-' . $ele; @endphp
-                                <div class="avail_hours @if(in_array($avail_hour, $availabilities)) active @endif" wire:click="toggleAvailItemStatus('{{$avail_hour}}')">{{$ele}}</div>
+                                @php
+                                $avail_hour = $item->short_name . '-' . $ele;
+                                $total_av[] = $avail_hour;
+                                $i++;
+                                @endphp
+                                <div class="avail_hours p-0 w-100">
+                                    <input type="checkbox" class="d-none" id="availability-{{$i}}" value="{{$avail_hour}}" x-model="availabilities" />
+                                    <label for="availability-{{$i}}" class="text-center py-1 w-100">{{$ele}}</label>
+                                </div>
                                 @empty
                                 <div>Not exist</div>
                                 @endforelse
                             </div>
                             @endforeach
                         </div>
-                        <div class="row mb-3">
-                            <div class="col-12 text-center">
-                                <button type="button" class="btn btn-info waves-effect waves-light btn-sm" wire:click="updateAvailabilities">Submit</button>
-                            </div>
+                    </div>
+                    <div class="row mb-3">
+                        <div class="col-12 text-center">
+                            <button type="button" class="btn btn-info waves-effect waves-light btn-sm" x-on:click="updateAvailabilities">Submit</button>
                         </div>
                     </div>
                 </div>
@@ -43,3 +51,27 @@
         </div>
     </div>
 </div>
+
+<script>
+    var total_availabilities = @json($total_av);
+    var availabilities = @json($availabilities);
+
+    var update_availabilities_init = () => {
+        Alpine.data('update_availabilities_init', () => ({
+            availabilities: availabilities,
+            selectAllAvailabilities(flag) {
+                if (!!flag) this.availabilities = total_availabilities;
+                else this.availabilities = [];
+            },
+            updateAvailabilities() {
+                @this.call('updateAvailabilities', this.availabilities);
+            }
+        }))
+    }
+    
+    if (typeof Alpine == 'undefined') {
+        document.addEventListener('alpine:init', () => { update_availabilities_init(); });
+    } else {
+        update_availabilities_init();
+    }
+</script>
